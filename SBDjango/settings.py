3698e788055084d72a500a03c83ba2b527dc3c5d
Beanie67 <stuart.j.bean@gmail.com>
2022-01-29 11:52:19 +1100

    Add Model

--- SBDjango/settings.py
+++ SBDjango/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app1.apps.App1Config',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
+MEDIA_URL = '/media/'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class APP(models.Model):
+    title = models.CharField(max_length=50)
+    summary = models.CharField(max_length=200)
+    detail = models.TextField(max_length=1000, default='')
+    image = models.ImageField(upload_to="images/")
+    # suggested that all models have by default a create and update date capture for filtering or sorting
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+#     def __str__(self):
+#         return self.summary
+#
+#     def detail_summary(self):
+#         return self.detail[:100]
+#
+#     class Meta:
+#         ordering = ['-updated_date']
+#
+# class JobAdmin(admin.ModelAdmin):
+#     readonly_fields = ('id',)
+#     list_display = ('id', 'title', 'summary', 'created_date', 'updated_date')
+#     fields = ('id', 'title', 'summary', 'detail', 'created_date', 'updated_date', 'image')
